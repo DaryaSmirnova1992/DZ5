96d869c429723a414420b49802436fa6739a1ffe
Darya <darecc@yandex.ru>
2022-08-25 00:09:15 +0400

    add Task 13

---
+++ Task_13/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите число");
+int num1 = int.Parse(Console.ReadLine());
+if(num1 > 99)
+{
+    string str = num1.ToString();
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.Write("Нет третьей цифры");
+}
