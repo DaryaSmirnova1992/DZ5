8f292bdaf1a89fb7267d2d2f408f15b1b7e4c714
Darya <darecc@yandex.ru>
2022-08-24 23:38:34 +0400

    add Task 10

---
+++ Task_10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите трехзначное число");
+int num1 = int.Parse(Console.ReadLine());
+if(99 < num1 && num1 < 1000)
+{
+    int dec = ( num1 - (num1 / 100) * 100 ) / 10;
+    Console.Write(dec);
+}
+else
+{
+    Console.Write("Fatal error! Введено не трехзначное число!");
+}
