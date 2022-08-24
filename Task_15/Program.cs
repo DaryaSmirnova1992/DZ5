21dbb1663b5130ec0fc52fd48f892a4c832b89e1
Darya <darecc@yandex.ru>
2022-08-25 00:39:53 +0400

    add Task 15

---
+++ Task_15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine ("Введите цифру, обозначающую день недели");
+int num1 = int.Parse(Console.ReadLine());
+if(num1 == 6 || num1 == 7)
+{
+Console.Write("да");
+}
+    else if(0 < num1 && num1 < 6 )
+    {
+    Console.Write("нет");
+    }
+else
+{
+Console.Write("Число не соответствует входным параметрам");
+}
